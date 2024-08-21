b3deaafc9671b9eb9fb78c58bb8e81594026e941
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 20:41:51 -0300

    Inicio Lista de Compras

---
+++ app/src/main/java/br/com/fiap/listadecompras/ItemAdapter.kt
@@ -0,0 +1,25 @@
+package br.com.fiap.listadecompras
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/br/com/fiap/listadecompras/ItemModel.kt
@@ -0,0 +1,4 @@
+package br.com.fiap.listadecompras
+
+data class ItemModel(val name: String) {
+}
